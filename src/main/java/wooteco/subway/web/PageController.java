fbffce6109ca76d096c3af92ea68b690ed006ae1
gracefulBrown <sunghyun.ryu@woowahan.com>
2020-05-17 18:40:18 +0900

    feature(favorite): init

--- src/main/java/wooteco/subway/web/PageController.java
+++ src/main/java/wooteco/subway/web/PageController.java
@@ -35,7 +35,32 @@ public class PageController {
     }
 
     @GetMapping(value = "/edges", produces = MediaType.TEXT_HTML_VALUE)
-    public String edgePage(Model model) {
+    public String edgePage() {
         return "admin/admin-edge";
+    }
+
+    @GetMapping(value = "/map", produces = MediaType.TEXT_HTML_VALUE)
+    public String mapPage() {
+        return "service/map";
+    }
+
+    @GetMapping(value = "/search", produces = MediaType.TEXT_HTML_VALUE)
+    public String searchPage() {
+        return "service/search";
+    }
+
+    @GetMapping(value = "/join", produces = MediaType.TEXT_HTML_VALUE)
+    public String joinPage() {
+        return "service/join";
+    }
+
+    @GetMapping(value = "/login", produces = MediaType.TEXT_HTML_VALUE)
+    public String loginPage() {
+        return "service/login";
+    }
+
+    @GetMapping(value = "/favorites", produces = MediaType.TEXT_HTML_VALUE)
+    public String favoritesPage() {
+        return "service/favorite";
     }
 }
