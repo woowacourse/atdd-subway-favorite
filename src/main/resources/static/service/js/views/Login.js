3d9e0eb072121878af9129e5c820cf072885061d
eastjun <eastjun@woowahan.com>
2020-05-19 08:12:33 +0900

    feat(login): add login page with ajax

--- src/main/resources/static/service/js/views/Login.js
+++ src/main/resources/static/service/js/views/Login.js
@@ -1,21 +1,39 @@
-import { EVENT_TYPE, ERROR_MESSAGE } from '../../utils/constants.js'
+import { EVENT_TYPE, ERROR_MESSAGE } from "../../utils/constants.js";
+import showSnackbar from "../../lib/snackbar/index.js";
+import api from "../../api/index.js";
 
 function Login() {
-  const $loginButton = document.querySelector('#login-button')
+  const $loginButton = document.querySelector("#login-button");
+  const $email = document.querySelector("#email");
+  const $password = document.querySelector("#password");
+
   const onLogin = event => {
-    event.preventDefault()
-    const emailValue = document.querySelector('#email').value
-    const passwordValue = document.querySelector('#password').value
-    if (!emailValue && !passwordValue) {
-      Snackbar.show({ text: ERROR_MESSAGE.LOGIN_FAIL, pos: 'bottom-center', showAction: false, duration: 2000 })
-      return
+    event.preventDefault();
+    const email = $email.value;
+    const password = $password.value;
+    if (!email && !password) {
+      showSnackbar(ERROR_MESSAGE.COMMON);
+      return;
     }
-  }
+    const loginMember = {
+      email: email,
+      password: password
+    };
+    api.member
+      .login(loginMember)
+      .then(jwt => {
+        if (!!jwt) {
+          localStorage.setItem("jwt", `${jwt.tokenType} ${jwt.accessToken}`);
+          location.href = "/";
+        }
+      })
+      .catch(error => console.log(error));
+  };
 
   this.init = () => {
-    $loginButton.addEventListener(EVENT_TYPE.CLICK, onLogin)
-  }
+    $loginButton.addEventListener(EVENT_TYPE.CLICK, onLogin);
+  };
 }
 
-const login = new Login()
-login.init()
+const login = new Login();
+login.init();
