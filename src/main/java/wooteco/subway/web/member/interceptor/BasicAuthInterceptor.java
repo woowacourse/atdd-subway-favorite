dc197be5a881627a094fff845059d161e44d00ca
moon <mjj920930@gmail.com>
2020-05-19 15:31:22 +0900

    feat: Basic HTTP Authentication 실습 완료

--- src/main/java/wooteco/subway/web/member/interceptor/BasicAuthInterceptor.java
+++ src/main/java/wooteco/subway/web/member/interceptor/BasicAuthInterceptor.java
@@ -1,5 +1,7 @@
 package wooteco.subway.web.member.interceptor;
 
+import java.util.Base64;
+
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import wooteco.subway.domain.member.Member;
@@ -22,12 +24,13 @@ public class BasicAuthInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        // TODO: Authorization 헤더를 통해 Basic 값을 추출 (authExtractor.extract() 메서드 활용)
+        String extractedAuth = authExtractor.extract(request, "basic");
 
-        // TODO: 추출한 Basic 값을 Base64를 통해 email과 password 값 추출(Base64.getDecoder().decode() 메서드 활용)
+        byte[] decodedAuth = Base64.getDecoder().decode(extractedAuth);
+        String[] authValues = new String(decodedAuth).split(":");
 
-        String email = "";
-        String password = "";
+        String email = authValues[0];
+        String password = authValues[1];
 
         Member member = memberService.findMemberByEmail(email);
         if (!member.checkPassword(password)) {
