4ce99ce02422ef998587fb3cfb2de023c1614810
giantim <giantim132@gmail.com>
2020-05-19 17:46:01 +0900

    feat: 로그인 프로세스 실습 구현

--- src/main/java/wooteco/subway/web/member/interceptor/BasicAuthInterceptor.java
+++ src/main/java/wooteco/subway/web/member/interceptor/BasicAuthInterceptor.java
@@ -9,6 +9,7 @@ import wooteco.subway.web.member.InvalidAuthenticationException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Base64;
 
 @Component
 public class BasicAuthInterceptor implements HandlerInterceptor {
@@ -22,12 +23,12 @@ public class BasicAuthInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        // TODO: Authorization 헤더를 통해 Basic 값을 추출 (authExtractor.extract() 메서드 활용)
+        String basic = authExtractor.extract(request, "Basic");
 
-        // TODO: 추출한 Basic 값을 Base64를 통해 email과 password 값 추출(Base64.getDecoder().decode() 메서드 활용)
-
-        String email = "";
-        String password = "";
+        byte[] decode = Base64.getDecoder().decode(basic);
+        String decodedBasic = new String(decode);
+        String email = decodedBasic.split(":")[0];
+        String password = decodedBasic.split(":")[1];
 
         Member member = memberService.findMemberByEmail(email);
         if (!member.checkPassword(password)) {
