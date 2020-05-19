2905d02382007d9bdb9ec6bb19caca8c8822a132
toneyparky <qkrtmddhks95@gmail.com>
2020-05-19 17:37:11 +0900

    practice: Bearer Authentication 인터셉터 실습

--- src/main/java/wooteco/subway/web/member/interceptor/BearerAuthInterceptor.java
+++ src/main/java/wooteco/subway/web/member/interceptor/BearerAuthInterceptor.java
@@ -1,10 +1,12 @@
 package wooteco.subway.web.member.interceptor;
 
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 import wooteco.subway.infra.JwtTokenProvider;
 import wooteco.subway.web.member.AuthorizationExtractor;
+import wooteco.subway.web.member.InvalidAuthenticationException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -22,13 +24,17 @@ public class BearerAuthInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request,
                              HttpServletResponse response, Object handler) {
-        // TODO: Authorization 헤더를 통해 Bearer 값을 추출 (authExtractor.extract() 메서드 활용)
+        String token = authExtractor.extract(request, "Bearer");
 
-        // TODO: 추출한 토큰값의 유효성 검사 (jwtTokenProvider.validateToken() 메서드 활용)
+        if (StringUtils.isEmpty(token)) {
+            throw new InvalidAuthenticationException("토큰이 없어요");
+        }
 
-        // TODO: 추출한 토큰값에서 email 정보 추출 (jwtTokenProvider.getSubject() 메서드 활용)
-        String email = "";
+        if (!jwtTokenProvider.validateToken(token)) {
+            throw new InvalidAuthenticationException("토큰이 이상해요");
+        }
 
+        String email = jwtTokenProvider.getSubject(token);
         request.setAttribute("loginMemberEmail", email);
         return true;
     }
--- src/test/java/wooteco/subway/AuthAcceptanceTest.java
+++ src/test/java/wooteco/subway/AuthAcceptanceTest.java
@@ -67,8 +67,15 @@ public class AuthAcceptanceTest extends AcceptanceTest {
 	}
 
 	public MemberResponse myInfoWithBearerAuth(TokenResponse tokenResponse) {
-		// TODO: oauth2 auth(bearer)를 활용하여 /me/bearer 요청하여 내 정보 조회
-		return null;
+		return given().auth().
+				oauth2(tokenResponse.getAccessToken()).
+				accept(MediaType.APPLICATION_JSON_VALUE).
+				when().
+				get("/me/bearer").
+				then().
+				log().all().
+				statusCode(HttpStatus.OK.value()).
+				extract().as(MemberResponse.class);
 	}
 
 	public TokenResponse login(String email, String password) {
