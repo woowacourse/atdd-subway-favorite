d5f966cb1a989fb5223a9bef3fd60ea721017d25
juyeongjung <juyeong.dev@gmail.com>
2020-05-19 16:25:31 +0900

    feat: 실습 - 로그인 프로세스 구현

--- src/main/java/wooteco/subway/web/member/interceptor/BearerAuthInterceptor.java
+++ src/main/java/wooteco/subway/web/member/interceptor/BearerAuthInterceptor.java
@@ -1,48 +1,49 @@
 package wooteco.subway.web.member.interceptor;
 
-import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
-import wooteco.subway.infra.JwtTokenProvider;
-import wooteco.subway.web.member.AuthorizationExtractor;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import wooteco.subway.infra.JwtTokenProvider;
+import wooteco.subway.web.member.AuthorizationExtractor;
+import wooteco.subway.web.member.InvalidAuthenticationException;
+
 @Component
 public class BearerAuthInterceptor implements HandlerInterceptor {
-    private AuthorizationExtractor authExtractor;
-    private JwtTokenProvider jwtTokenProvider;
+	private AuthorizationExtractor authExtractor;
+	private JwtTokenProvider jwtTokenProvider;
 
-    public BearerAuthInterceptor(AuthorizationExtractor authExtractor, JwtTokenProvider jwtTokenProvider) {
-        this.authExtractor = authExtractor;
-        this.jwtTokenProvider = jwtTokenProvider;
-    }
+	public BearerAuthInterceptor(AuthorizationExtractor authExtractor, JwtTokenProvider jwtTokenProvider) {
+		this.authExtractor = authExtractor;
+		this.jwtTokenProvider = jwtTokenProvider;
+	}
 
-    @Override
-    public boolean preHandle(HttpServletRequest request,
-                             HttpServletResponse response, Object handler) {
-        // TODO: Authorization 헤더를 통해 Bearer 값을 추출 (authExtractor.extract() 메서드 활용)
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+		String token = authExtractor.extract(request, "Bearer");
 
-        // TODO: 추출한 토큰값의 유효성 검사 (jwtTokenProvider.validateToken() 메서드 활용)
+		if (!jwtTokenProvider.validateToken(token)) {
+			throw new InvalidAuthenticationException("올바르지 않은 토큰 입니다.");
+		}
+		String email = jwtTokenProvider.getSubject(token);
+		request.setAttribute("loginMemberEmail", email);
+		return true;
+	}
 
-        // TODO: 추출한 토큰값에서 email 정보 추출 (jwtTokenProvider.getSubject() 메서드 활용)
-        String email = "";
+	@Override
+	public void postHandle(HttpServletRequest request,
+		HttpServletResponse response,
+		Object handler,
+		ModelAndView modelAndView) throws Exception {
 
-        request.setAttribute("loginMemberEmail", email);
-        return true;
-    }
+	}
 
-    @Override
-    public void postHandle(HttpServletRequest request,
-                           HttpServletResponse response,
-                           Object handler,
-                           ModelAndView modelAndView) throws Exception {
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
+		Exception ex) throws Exception {
 
-    }
-
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-
-    }
+	}
 }
