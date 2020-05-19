5dda1f2a6b888158774d34d2de59d049d5d3e2db
moon <mjj920930@gmail.com>
2020-05-19 16:02:51 +0900

    feat: FormAuth 및 세션을 활용한 인증 실습 완료

--- src/test/java/wooteco/subway/AuthAcceptanceTest.java
+++ src/test/java/wooteco/subway/AuthAcceptanceTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
+import io.restassured.authentication.FormAuthConfig;
 import wooteco.subway.service.member.dto.MemberResponse;
 import wooteco.subway.service.member.dto.TokenResponse;
 
@@ -64,13 +65,28 @@ public class AuthAcceptanceTest extends AcceptanceTest {
 	}
 
 	public MemberResponse myInfoWithSession(String email, String password) {
-		// TODO: form auth를 활용하여 /me/session 요청하여 내 정보 조회
-		return null;
+		return given().
+			auth().form(email, password, new FormAuthConfig("/login", "email", "password")).
+			accept(MediaType.APPLICATION_JSON_VALUE).
+			when().
+			get("/me/session").
+			then().
+			log().all().
+			statusCode(HttpStatus.OK.value()).
+			extract().as(MemberResponse.class);
 	}
 
 	public MemberResponse myInfoWithBearerAuth(TokenResponse tokenResponse) {
 		// TODO: oauth2 auth(bearer)를 활용하여 /me/bearer 요청하여 내 정보 조회
-		return null;
+		return given().
+			auth().form("", "").
+			accept(MediaType.APPLICATION_JSON_VALUE).
+			when().
+			get("/me/bearer").
+			then().
+			log().all().
+			statusCode(HttpStatus.OK.value()).
+			extract().as(MemberResponse.class);
 	}
 
 	public TokenResponse login(String email, String password) {
