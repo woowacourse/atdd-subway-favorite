d624a3cb5336369ef6cd8998234ff291b6b1f2a8
juyeongjung <juyeong.dev@gmail.com>
2020-05-19 14:31:55 +0900

    feat: 문서자동화 실습

--- src/test/java/wooteco/subway/web/line/LineControllerTest.java
+++ src/test/java/wooteco/subway/web/line/LineControllerTest.java
@@ -3,7 +3,9 @@ package wooteco.subway.web.line;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
@@ -25,7 +27,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(controllers = LineController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 @Import(ETagHeaderFilter.class)
 public class LineControllerTest {
     @MockBean
