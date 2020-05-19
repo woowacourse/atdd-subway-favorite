cb23dd9970b1c6c2c31e52545b0fa9a069e6bd7b
김예림 <kiel0103@naver.com>
2020-05-19 20:03:40 +0900

    feat: Bearer Auth 실습

--- src/main/java/wooteco/subway/web/member/interceptor/BearerAuthInterceptor.java
+++ src/main/java/wooteco/subway/web/member/interceptor/BearerAuthInterceptor.java
@@ -1,13 +1,12 @@
 package wooteco.subway.web.member.interceptor;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 import wooteco.subway.infra.JwtTokenProvider;
 import wooteco.subway.web.member.AuthorizationExtractor;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @Component
 public class BearerAuthInterceptor implements HandlerInterceptor {
@@ -23,11 +22,16 @@ public class BearerAuthInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request,
                              HttpServletResponse response, Object handler) {
         // TODO: Authorization 헤더를 통해 Bearer 값을 추출 (authExtractor.extract() 메서드 활용)
+        String bearer = authExtractor.extract(request, "BEARER");
 
         // TODO: 추출한 토큰값의 유효성 검사 (jwtTokenProvider.validateToken() 메서드 활용)
+        if (!jwtTokenProvider.validateToken(bearer)) {
+            return false;
+        }
 
         // TODO: 추출한 토큰값에서 email 정보 추출 (jwtTokenProvider.getSubject() 메서드 활용)
-        String email = "";
+
+        String email = jwtTokenProvider.getSubject(bearer);
 
         request.setAttribute("loginMemberEmail", email);
         return true;
