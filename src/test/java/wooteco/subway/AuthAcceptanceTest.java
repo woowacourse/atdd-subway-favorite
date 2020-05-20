89d033fdec101c7b07302a4ad0f738b2a3c64bad
hongbin23 <khbasd@gmail.com>
2020-05-20 10:52:58 +0900

    refactor: 불필요한 메서드 제거

--- src/test/java/wooteco/subway/AuthAcceptanceTest.java
+++ src/test/java/wooteco/subway/AuthAcceptanceTest.java
@@ -1,6 +1,5 @@
 package wooteco.subway;
 
-import static io.restassured.RestAssured.*;
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.HashMap;
@@ -11,101 +10,51 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
-import io.restassured.authentication.FormAuthConfig;
 import wooteco.subway.service.member.dto.MemberResponse;
 import wooteco.subway.service.member.dto.TokenResponse;
 
 public class AuthAcceptanceTest extends AcceptanceTest {
-    @DisplayName("Basic Auth")
-    @Test
-    void myInfoWithBasicAuth() {
-        createMember(TEST_USER_EMAIL, TEST_USER_NAME, TEST_USER_PASSWORD);
+	@DisplayName("Bearer Auth")
+	@Test
+	void myInfoWithBearerAuth() {
+		createMember(TEST_USER_EMAIL, TEST_USER_NAME, TEST_USER_PASSWORD);
+		TokenResponse tokenResponse = login(TEST_USER_EMAIL, TEST_USER_PASSWORD);
 
-        MemberResponse memberResponse = myInfoWithBasicAuth(TEST_USER_EMAIL, TEST_USER_PASSWORD);
+		MemberResponse memberResponse = myInfoWithBearerAuth(tokenResponse);
+		assertThat(memberResponse.getId()).isNotNull();
+		assertThat(memberResponse.getEmail()).isEqualTo(TEST_USER_EMAIL);
+		assertThat(memberResponse.getName()).isEqualTo(TEST_USER_NAME);
+	}
 
-        assertThat(memberResponse.getId()).isNotNull();
-        assertThat(memberResponse.getEmail()).isEqualTo(TEST_USER_EMAIL);
-        assertThat(memberResponse.getName()).isEqualTo(TEST_USER_NAME);
-    }
+	public MemberResponse myInfoWithBearerAuth(TokenResponse tokenResponse) {
+		return
+			given().
+				accept(MediaType.APPLICATION_JSON_VALUE).
+				auth().
+				preemptive().
+				oauth2(tokenResponse.getAccessToken()).
+				when().
+				get("/me/bearer").
+				then().
+				log().all().
+				extract().as(MemberResponse.class);
+	}
 
-    @DisplayName("Session")
-    @Test
-    void myInfoWithSession() {
-        createMember(TEST_USER_EMAIL, TEST_USER_NAME, TEST_USER_PASSWORD);
+	public TokenResponse login(String email, String password) {
+		Map<String, String> params = new HashMap<>();
+		params.put("email", email);
+		params.put("password", password);
 
-        MemberResponse memberResponse = myInfoWithSession(TEST_USER_EMAIL, TEST_USER_PASSWORD);
-
-        assertThat(memberResponse.getId()).isNotNull();
-        assertThat(memberResponse.getEmail()).isEqualTo(TEST_USER_EMAIL);
-        assertThat(memberResponse.getName()).isEqualTo(TEST_USER_NAME);
-    }
-
-    @DisplayName("Bearer Auth")
-    @Test
-    void myInfoWithBearerAuth() {
-        createMember(TEST_USER_EMAIL, TEST_USER_NAME, TEST_USER_PASSWORD);
-        TokenResponse tokenResponse = login(TEST_USER_EMAIL, TEST_USER_PASSWORD);
-
-        MemberResponse memberResponse = myInfoWithBearerAuth(tokenResponse);
-        assertThat(memberResponse.getId()).isNotNull();
-        assertThat(memberResponse.getEmail()).isEqualTo(TEST_USER_EMAIL);
-        assertThat(memberResponse.getName()).isEqualTo(TEST_USER_NAME);
-    }
-
-    public MemberResponse myInfoWithBasicAuth(String email, String password) {
-        return
-            given()
-                .auth()
-                .preemptive()
-                .basic(email, password)
-                .when().
-                get("/me/basic").
-                then().
-                log().all().
-                extract().as(MemberResponse.class);
-    }
-
-    public MemberResponse myInfoWithSession(String email, String password) {
-        return
-            given()
-                .auth().form(email, password, new FormAuthConfig("login", "email", "password"))
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-            .when()
-            .get("/me/session")
-            .then()
-            .log().all()
-            .extract().as(MemberResponse.class);
-    }
-
-    public MemberResponse myInfoWithBearerAuth(TokenResponse tokenResponse) {
-        return
-            given().
-                accept(MediaType.APPLICATION_JSON_VALUE).
-                auth().
-                preemptive().
-                oauth2(tokenResponse.getAccessToken()).
-                when().
-                get("/me/bearer").
-                then().
-                log().all().
-                extract().as(MemberResponse.class);
-    }
-
-    public TokenResponse login(String email, String password) {
-        Map<String, String> params = new HashMap<>();
-        params.put("email", email);
-        params.put("password", password);
-
-        return
-            given().
-                body(params).
-                contentType(MediaType.APPLICATION_JSON_VALUE).
-                accept(MediaType.APPLICATION_JSON_VALUE).
-                when().
-                post("/oauth/token").
-                then().
-                log().all().
-                statusCode(HttpStatus.OK.value()).
-                extract().as(TokenResponse.class);
-    }
+		return
+			given().
+				body(params).
+				contentType(MediaType.APPLICATION_JSON_VALUE).
+				accept(MediaType.APPLICATION_JSON_VALUE).
+				when().
+				post("/oauth/token").
+				then().
+				log().all().
+				statusCode(HttpStatus.OK.value()).
+				extract().as(TokenResponse.class);
+	}
 }
