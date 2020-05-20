0ec1cff091b7c52c41ccdd022219a53fcba251b9
Eun Seok <jeseok95@gmail.com>
2020-05-20 15:56:30 +0900

    [refactor] 불필요한 interceptor 제거

--- src/main/java/wooteco/subway/web/member/LoginMemberController.java
+++ src/main/java/wooteco/subway/web/member/LoginMemberController.java
@@ -1,16 +1,11 @@
 package wooteco.subway.web.member;
 
-import java.util.Map;
-
-import javax.servlet.http.HttpSession;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import wooteco.subway.domain.member.Member;
@@ -22,46 +17,33 @@ import wooteco.subway.service.member.dto.UpdateMemberRequest;
 
 @RestController
 public class LoginMemberController {
-	private MemberService memberService;
+    private MemberService memberService;
 
-	public LoginMemberController(MemberService memberService) {
-		this.memberService = memberService;
-	}
+    public LoginMemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
 
-	@PostMapping("/oauth/token")
-	public ResponseEntity<TokenResponse> login(@RequestBody LoginRequest param) {
-		String token = memberService.createToken(param);
-		return ResponseEntity.ok().body(new TokenResponse(token, "bearer"));
-	}
+    @PostMapping("/oauth/token")
+    public ResponseEntity<TokenResponse> login(@RequestBody LoginRequest param) {
+        String token = memberService.createToken(param);
+        return ResponseEntity.ok().body(new TokenResponse(token, "bearer"));
+    }
 
-	@PostMapping("/login")
-	public ResponseEntity login(@RequestParam Map<String, String> paramMap, HttpSession session) {
-		String email = paramMap.get("email");
-		String password = paramMap.get("password");
-		if (!memberService.loginWithForm(email, password)) {
-			throw new InvalidAuthenticationException("올바르지 않은 이메일과 비밀번호 입력");
-		}
+    @GetMapping("/me/bearer")
+    public ResponseEntity<MemberResponse> getMemberOfMineBasic(@LoginMember Member member) {
+        return ResponseEntity.ok().body(MemberResponse.of(member));
+    }
 
-		session.setAttribute("loginMemberEmail", email);
+    @PutMapping("/me/bearer")
+    public ResponseEntity<Void> updateMemberOfMineBearer(@LoginMember Member member,
+        @RequestBody UpdateMemberRequest updateMemberRequest) {
+        memberService.updateMember(member.getId(), updateMemberRequest);
+        return ResponseEntity.ok().build();
+    }
 
-		return ResponseEntity.ok().build();
-	}
-
-	@GetMapping({"/me/basic", "/me/session", "/me/bearer"})
-	public ResponseEntity<MemberResponse> getMemberOfMineBasic(@LoginMember Member member) {
-		return ResponseEntity.ok().body(MemberResponse.of(member));
-	}
-
-	@PutMapping("/me/bearer")
-	public ResponseEntity<Void> updateMemberOfMineBearer(@LoginMember Member member,
-		@RequestBody UpdateMemberRequest updateMemberRequest) {
-		memberService.updateMember(member.getId(), updateMemberRequest);
-		return ResponseEntity.ok().build();
-	}
-
-	@DeleteMapping("/me/bearer")
-	public ResponseEntity<Void> deleteMemberOfMineBearer(@LoginMember Member member) {
-		memberService.deleteMember(member.getId());
-		return ResponseEntity.ok().build();
-	}
+    @DeleteMapping("/me/bearer")
+    public ResponseEntity<Void> deleteMemberOfMineBearer(@LoginMember Member member) {
+        memberService.deleteMember(member.getId());
+        return ResponseEntity.ok().build();
+    }
 }
--- src/test/java/wooteco/subway/AuthAcceptanceTest.java
+++ src/test/java/wooteco/subway/AuthAcceptanceTest.java
@@ -15,29 +15,6 @@ import wooteco.subway.service.member.dto.MemberResponse;
 import wooteco.subway.service.member.dto.TokenResponse;
 
 public class AuthAcceptanceTest extends AcceptanceTest {
-	@DisplayName("Basic Auth")
-	@Test
-	void myInfoWithBasicAuth() {
-		createMember(TEST_USER_EMAIL, TEST_USER_NAME, TEST_USER_PASSWORD);
-
-		MemberResponse memberResponse = myInfoWithBasicAuth(TEST_USER_EMAIL, TEST_USER_PASSWORD);
-
-		assertThat(memberResponse.getId()).isNotNull();
-		assertThat(memberResponse.getEmail()).isEqualTo(TEST_USER_EMAIL);
-		assertThat(memberResponse.getName()).isEqualTo(TEST_USER_NAME);
-	}
-
-	@DisplayName("Session")
-	@Test
-	void myInfoWithSession() {
-		createMember(TEST_USER_EMAIL, TEST_USER_NAME, TEST_USER_PASSWORD);
-
-		MemberResponse memberResponse = myInfoWithSession(TEST_USER_EMAIL, TEST_USER_PASSWORD);
-
-		assertThat(memberResponse.getId()).isNotNull();
-		assertThat(memberResponse.getEmail()).isEqualTo(TEST_USER_EMAIL);
-		assertThat(memberResponse.getName()).isEqualTo(TEST_USER_NAME);
-	}
 
 	@DisplayName("Bearer Auth")
 	@Test
@@ -51,33 +28,6 @@ public class AuthAcceptanceTest extends AcceptanceTest {
 		assertThat(memberResponse.getName()).isEqualTo(TEST_USER_NAME);
 	}
 
-	public MemberResponse myInfoWithBasicAuth(String email, String password) {
-		return given().auth()
-			.preemptive()
-			.basic(email, password)
-			.accept(MediaType.APPLICATION_JSON_VALUE)
-			.when()
-			.get("/me/basic")
-			.then()
-			.log().all()
-			.statusCode(HttpStatus.OK.value())
-			.extract().as(MemberResponse.class);
-	}
-
-	public MemberResponse myInfoWithSession(String email, String password) {
-		return given().auth()
-			.form(
-				email,
-				password,
-				new FormAuthConfig("/login", "email", "password"))
-			.accept(MediaType.APPLICATION_JSON_VALUE)
-			.when()
-			.get("/me/session")
-			.then()
-			.log().all()
-			.statusCode(HttpStatus.OK.value())
-			.extract().as(MemberResponse.class);
-	}
 
 	public MemberResponse myInfoWithBearerAuth(TokenResponse tokenResponse) {
 		return given().auth()
