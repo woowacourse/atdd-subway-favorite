0df34398a7bce84932761f99f77767f8ef11928c
Andy H. Jung <commaniakr@gmail.com>
2020-05-20 16:14:58 +0900

    feat: 문서 자동화 구현

--- src/main/java/wooteco/subway/web/member/LoginMemberController.java
+++ src/main/java/wooteco/subway/web/member/LoginMemberController.java
@@ -38,7 +38,7 @@ public class LoginMemberController {
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping({"/me/basic", "/me/session", "/me/bearer"})
+    @GetMapping("/me/bearer")
     public ResponseEntity<MemberResponse> getMemberOfMineBasic(@LoginMember Member member) {
         return ResponseEntity.ok().body(MemberResponse.of(member));
     }
