763fbce5df5e1d5c8cb46bb915a14a491b714bc0
ordinCode <ysh1346@naver.com>
2020-05-20 12:07:56 +0900

    feat Join.js
     - 재입력한 비밀번호 확인 추가

--- src/main/resources/static/service/js/views/Join.js
+++ src/main/resources/static/service/js/views/Join.js
@@ -3,18 +3,19 @@ import api from '../../api/index.js'
 
 function Join() {
     const $joinButton = document.querySelector('#join-submit');
+    const $passwordCheck = document.querySelector('#password-check');
+    const $password = document.querySelector('#password');
+    const $passwordCheckMessage = document.querySelector("#password-check-message");
 
     const onJoin = event => {
         event.preventDefault();
         const emailValue = document.querySelector('#email').value;
         const nameValue = document.querySelector('#name').value;
-        const passwordValue = document.querySelector('#password').value;
-        const passwordCheckValue = document.querySelector('#password-check').value;
 
         const param = {
             email: emailValue,
             name: nameValue,
-            password: passwordValue
+            password: $password.value
         }
 
         api.member.create(param).then(response => {
@@ -26,8 +27,22 @@ function Join() {
             error.text.then(error => alert(error)))
     }
 
+    const validatePassword = event => {
+        console.log($passwordCheck.value);
+        if ($password.value === $passwordCheck.value) {
+            console.log("일치")
+            $joinButton.removeAttribute("disabled");
+            $passwordCheckMessage.hidden = true;
+            return
+        }
+        $joinButton.disabled = true;
+        $passwordCheckMessage.removeAttribute("hidden");
+    }
+
     this.init = () => {
         $joinButton.addEventListener(EVENT_TYPE.CLICK, onJoin)
+        $passwordCheck.addEventListener("keyup", validatePassword)
+
     }
 }
 
