390dcd39a9d9fee438c93b5af6bf4f06ff7898f6
toneyparky <qkrtmddhks95@gmail.com>
2020-05-20 11:47:04 +0900

    feat: 클라이언트 회원가입 폼 검증
    /snackbar: alert 대신 사용할 snackbar 생성
    Join.js: 유효성 검사 메서드 생성

---
+++ src/main/resources/static/service/js/views/Join.js
@@ -0,0 +1,52 @@
+import {ERROR_MESSAGE, EVENT_TYPE} from '../../utils/constants.js'
+import showSnackbar from "../../lib/snackbar/index.js";
+
+const validateEmail = (email) => {
+  const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+  return re.test(String(email).toLowerCase())
+}
+
+const validate = ({emailValue, nameValue, passwordValue, passwordCheckValue}) => {
+  if (!emailValue && !nameValue && !passwordValue && !passwordCheckValue) {
+    showSnackbar(ERROR_MESSAGE.JOIN_EMPTY_VALUE);
+    return
+  }
+
+  if (!validateEmail(emailValue)) {
+    showSnackbar(ERROR_MESSAGE.JOIN_INVALID_EMAIL);
+    return
+  }
+
+  if (passwordValue !== passwordCheckValue) {
+    showSnackbar(ERROR_MESSAGE.JOIN_PASSWORD_NOT_EQUALS);
+    return
+  }
+}
+
+function Join() {
+  const $joinButton = document.querySelector('#join-button')
+  const onJoin = event => {
+    event.preventDefault()
+    const emailValue = document.querySelector('#email').value
+    const nameValue = document.querySelector('#name').value
+    const passwordValue = document.querySelector('#password').value
+    const passwordCheckValue = document.querySelector('#password-check').value
+
+    const data = {
+      emailValue: emailValue,
+      nameValue: nameValue,
+      passwordValue: passwordValue,
+      passwordCheckValue: passwordCheckValue
+    }
+
+    validate(data)
+  }
+
+  this.init = () => {
+    $joinButton.addEventListener(EVENT_TYPE.CLICK, onJoin)
+  }
+}
+
+
+const join = new Join()
+join.init()
--- src/main/resources/static/service/utils/constants.js
+++ src/main/resources/static/service/utils/constants.js
@@ -3,6 +3,9 @@ export const EVENT_TYPE = {
 }
 
 export const ERROR_MESSAGE = {
+  JOIN_EMPTY_VALUE: '😭 입력되지 않은 정보가 있습니다. 다시 시도해주세요.',
+  JOIN_PASSWORD_NOT_EQUALS: '😭 비밀번호와 비밀번호 확인이 일치하지 않습니다. 다시 시도해주세요.',
+  JOIN_INVALID_EMAIL: '😭 Email 형식이 올바르지 않습니다. 다시 시도해주세요.',
   LOGIN_FAIL: '😭 로그인이 실패했습니다. 다시 시도해주세요.'
 }
 
