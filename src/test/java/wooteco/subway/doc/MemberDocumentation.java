3e85e1f608f4f6be0ea9fde877c6e876bdd26689
hamliet <hamliet@hanyang.ac.kr>
2020-05-21 17:09:35 +0900

    feat: MemberController find() 테스트 작성

--- src/test/java/wooteco/subway/doc/MemberDocumentation.java
+++ src/test/java/wooteco/subway/doc/MemberDocumentation.java
@@ -1,29 +1,43 @@
 package wooteco.subway.doc;
 
-import static org.springframework.restdocs.headers.HeaderDocumentation.*;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
-import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-
 import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
 import org.springframework.restdocs.payload.JsonFieldType;
 
-public class MemberDocumentation {
-	public static RestDocumentationResultHandler createMember() {
-		return document("members/create");
-	}
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 
-	public static RestDocumentationResultHandler updateMember() {
-		return document("members/update",
-			requestFields(
-				fieldWithPath("name").type(JsonFieldType.STRING)
-					.description("The user's name"),
-				fieldWithPath("password").type(JsonFieldType.STRING)
-					.description("The user's password")
-			),
-			requestHeaders(
-				headerWithName("Authorization").description(
-					"The token for login which is Bearer Type")
-			)
-		);
-	}
+public class MemberDocumentation {
+    public static RestDocumentationResultHandler createMember() {
+        return document("members/create",
+                requestFields(
+                        fieldWithPath("email").type(JsonFieldType.STRING)
+                                .description("The user's e-mail"),
+                        fieldWithPath("name").type(JsonFieldType.STRING)
+                                .description("The user's name"),
+                        fieldWithPath("password").type(JsonFieldType.STRING)
+                                .description("The user's password")
+                ));
+    }
+
+    public static RestDocumentationResultHandler findMember() {
+        return document("members/find");
+    }
+
+    public static RestDocumentationResultHandler updateMember() {
+        return document("members/update",
+                requestFields(
+                        fieldWithPath("name").type(JsonFieldType.STRING)
+                                .description("The user's name"),
+                        fieldWithPath("password").type(JsonFieldType.STRING)
+                                .description("The user's password")
+                ),
+                requestHeaders(
+                        headerWithName("Authorization").description(
+                                "The token for login which is Bearer Type")
+                )
+        );
+    }
 }
--- src/test/java/wooteco/subway/web/member/MemberControllerTest.java
+++ src/test/java/wooteco/subway/web/member/MemberControllerTest.java
@@ -1,13 +1,6 @@
 package wooteco.subway.web.member;
 
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.BDDMockito.given;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static wooteco.subway.AcceptanceTest.*;
-
+import com.google.gson.Gson;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -22,10 +15,20 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
-
 import wooteco.subway.doc.MemberDocumentation;
 import wooteco.subway.domain.member.Member;
 import wooteco.subway.service.member.MemberService;
+import wooteco.subway.service.member.dto.MemberResponse;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static wooteco.subway.AcceptanceTest.*;
 
 @ExtendWith(RestDocumentationExtension.class)
 @SpringBootTest
@@ -38,13 +41,16 @@ public class MemberControllerTest {
 	@Autowired
 	protected MockMvc mockMvc;
 
+	private Gson gson;
+
 	@BeforeEach
 	public void setUp(WebApplicationContext webApplicationContext,
-		RestDocumentationContextProvider restDocumentation) {
+					  RestDocumentationContextProvider restDocumentation) {
 		this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
-			.addFilter(new ShallowEtagHeaderFilter())
-			.apply(documentationConfiguration(restDocumentation))
-			.build();
+				.addFilter(new ShallowEtagHeaderFilter())
+				.apply(documentationConfiguration(restDocumentation))
+				.build();
+		gson = new Gson();
 	}
 
 	@Test
@@ -67,12 +73,19 @@ public class MemberControllerTest {
 	}
 
 	@Test
-	public void getMember() {
+	public void getMember() throws Exception {
 		Member member = new Member(1L, TEST_USER_EMAIL, TEST_USER_NAME,
-			TEST_USER_PASSWORD);
+				TEST_USER_PASSWORD);
+		MemberResponse memberResponse = MemberResponse.of(member);
 		given(memberService.findMemberByEmail(TEST_USER_EMAIL)).willReturn(member);
 
-		// TODO 남은 부분 추가
+		this.mockMvc.perform(get("/members")
+				.param("email", TEST_USER_EMAIL)
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().json(gson.toJson(memberResponse)))
+				.andDo(print())
+				.andDo(MemberDocumentation.findMember());
 	}
 
 }
