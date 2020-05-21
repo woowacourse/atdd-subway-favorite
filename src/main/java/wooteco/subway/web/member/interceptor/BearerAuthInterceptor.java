0b25e2ec1252708737aff1f95f723f8e026b282d
Ramen6315 <ramen6315@gmail.com>
2020-05-21 18:16:09 +0900

    feat: BearerAuthInterceptor preHandle 구현

--- src/main/java/wooteco/subway/web/member/interceptor/BearerAuthInterceptor.java
+++ src/main/java/wooteco/subway/web/member/interceptor/BearerAuthInterceptor.java
@@ -5,6 +5,7 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 import wooteco.subway.infra.JwtTokenProvider;
 import wooteco.subway.web.member.AuthorizationExtractor;
+import wooteco.subway.web.member.InvalidAuthenticationException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -22,13 +23,11 @@ public class BearerAuthInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request,
                              HttpServletResponse response, Object handler) {
-        // TODO: Authorization 헤더를 통해 Bearer 값을 추출 (authExtractor.extract() 메서드 활용)
-
-        // TODO: 추출한 토큰값의 유효성 검사 (jwtTokenProvider.validateToken() 메서드 활용)
-
-        // TODO: 추출한 토큰값에서 email 정보 추출 (jwtTokenProvider.getSubject() 메서드 활용)
-        String email = "";
-
+        String bearer = authExtractor.extract(request, "Bearer");
+        if(!jwtTokenProvider.validateToken(bearer)) {
+            throw new InvalidAuthenticationException();
+        }
+        String email =  jwtTokenProvider.getSubject(bearer);
         request.setAttribute("loginMemberEmail", email);
         return true;
     }
