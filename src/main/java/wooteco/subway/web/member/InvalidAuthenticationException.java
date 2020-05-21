ca17a220066fe934f316cd20267c627cb34da7bc
Ramen6315 <ramen6315@gmail.com>
2020-05-21 18:16:42 +0900

    refactor: InvalidAuthenticationException 생성자 수정

--- src/main/java/wooteco/subway/web/member/InvalidAuthenticationException.java
+++ src/main/java/wooteco/subway/web/member/InvalidAuthenticationException.java
@@ -5,7 +5,13 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.UNAUTHORIZED)
 public class InvalidAuthenticationException extends RuntimeException{
+    private static final String MESSAGE = "잘못된 토큰 값 입니다.";
+
     public InvalidAuthenticationException(String message) {
         super(message);
+    }
+
+    public InvalidAuthenticationException() {
+        super(MESSAGE);
     }
 }
