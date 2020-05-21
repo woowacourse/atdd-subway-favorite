49b9688e87fd571198aa3ca0940eccd527adff13
Ramen6315 <ramen6315@gmail.com>
2020-05-21 16:39:16 +0900

    test: Service Test 작성

---
+++ src/test/java/wooteco/subway/service/member/MemberServiceIntegrationTest.java
@@ -0,0 +1,51 @@
+package wooteco.subway.service.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import wooteco.subway.domain.member.Member;
+import wooteco.subway.domain.member.MemberRepository;
+import wooteco.subway.infra.JwtTokenProvider;
+import wooteco.subway.service.member.dto.LoginRequest;
+import wooteco.subway.web.member.exception.NotExistMemberDataException;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+public class MemberServiceIntegrationTest {
+    public static final String TEST_USER_EMAIL = "brown@email.com";
+    public static final String TEST_USER_PASSWORD = "brown";
+    private MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @MockBean
+    JwtTokenProvider jwtTokenProvider;
+
+    @BeforeEach
+    void setUp() {
+        this.memberService = new MemberService(memberRepository, jwtTokenProvider);
+    }
+
+    @DisplayName("로그인 email 정보가 없을 경우 익셉션이 발생한다")
+    @Test
+    void createTokenFail() {
+        LoginRequest loginRequest = new LoginRequest(TEST_USER_EMAIL, TEST_USER_PASSWORD);
+
+        Assertions.assertThatThrownBy(() -> memberService.createToken(loginRequest))
+                .isInstanceOf(NotExistMemberDataException.class);
+    }
+
+}
