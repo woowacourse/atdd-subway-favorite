0cc4e84fa0a19de2a9c8d0341ea7b65be9681783
moonyoungCHAE <xpf_fl@naver.com>
2020-05-21 14:03:15 +0900

    test: Member Controller create 테스트 및 문서화

--- src/test/java/wooteco/subway/web/member/MemberControllerTest.java
+++ src/test/java/wooteco/subway/web/member/MemberControllerTest.java
@@ -1,5 +1,78 @@
 package wooteco.subway.web.member;
 
+import com.google.gson.Gson;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.RestDocsAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentationConfigurer;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+import wooteco.subway.doc.MemberDocumentation;
+import wooteco.subway.domain.member.Member;
+import wooteco.subway.service.member.MemberService;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(RestDocumentationExtension.class)
+@WebMvcTest(controllers = MemberController.class)
 public class MemberControllerTest {
-    // TODO: 회원가입 API 테스트
+    private static final String TEST_EMAIL = "abc@naver.com";
+    private static final String TEST_NAME = "brown";
+    private static final String TEST_PASSWORD = "password";
+
+    private Gson gson = new Gson();
+
+    @MockBean
+    private BearerAuthInterceptor bearerAuthInterceptor;
+
+    @MockBean
+    private MemberService memberService;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .addFilter(new ShallowEtagHeaderFilter())
+                .apply(documentationConfiguration(restDocumentation))
+                .build();
+    }
+
+    @Test
+    void createMember() throws Exception {
+        Member member = new Member(TEST_EMAIL,TEST_NAME, TEST_PASSWORD);
+        given(memberService.createMember(any())).willReturn(member);
+
+        Map<String, String> params = new LinkedHashMap<>();
+        params.put("email", TEST_EMAIL);
+        params.put("name", TEST_NAME);
+        params.put("password", TEST_PASSWORD);
+
+        String request = gson.toJson(params);
+        mockMvc.perform(post("/members")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(request))
+                .andExpect(status().isCreated())
+                .andDo(print())
+                .andDo(MemberDocumentation.createMember());
+    }
 }
