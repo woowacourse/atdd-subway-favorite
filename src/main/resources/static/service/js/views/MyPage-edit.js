ced9b0da3727a8f3d685a8cbc8e468e629774fa7
Tak Hyungmin <milkho11@naver.com>
2020-05-22 22:38:19 +0900

    feat: 내 정보 수정 API 연결

---
+++ src/main/resources/static/service/js/views/MyPage-edit.js
@@ -0,0 +1,65 @@
+import {EVENT_TYPE} from '../../utils/constants.js'
+
+function MyPageEdit() {
+    const $emailValue = document.querySelector('#email');
+    const $passwordValue = document.querySelector('#password');
+    const $nameValue = document.querySelector('#name');
+    const $updateButton = document.querySelector('#update-my-info-button');
+
+    const editButtonEventHandler = event => {
+        event.preventDefault();
+        fetch("/members/" + $emailValue.dataset.id, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': "Bearer " + sessionStorage.getItem("token")
+            },
+            body: JSON.stringify({
+                name: $nameValue.value,
+                password: $passwordValue.value
+            })
+        })
+            .then(data => data.json())
+            .then(data => {
+                    alert(data);
+                }
+            )
+    }
+
+    function requestMyPageData(email, token) {
+        fetch("/members?email=" + email, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': "Bearer " + token
+            }
+        })
+            .then(data => data.json())
+            .then(data => {
+                    $emailValue.dataset.id = data.id;
+                    $emailValue.value = data.email;
+                    $nameValue.value = data.name;
+                }
+            )
+    }
+
+    this.loadMyPage = () => {
+        const token = sessionStorage.getItem("token");
+        if (token === null) {
+            alert("로그인 하고 다시 오세요.");
+            return;
+        }
+        const email = sessionStorage.getItem("email");
+
+        requestMyPageData(email, token);
+    }
+
+
+    this.init = () => {
+        $updateButton.addEventListener(EVENT_TYPE.CLICK, editButtonEventHandler)
+    }
+}
+
+const myPageEdit = new MyPageEdit();
+myPageEdit.loadMyPage();
+myPageEdit.init();
