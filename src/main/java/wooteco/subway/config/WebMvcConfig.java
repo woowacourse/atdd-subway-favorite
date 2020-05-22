82cc7e95db1305ff5469660a759c9af4af11934c
Tak Hyungmin <milkho11@naver.com>
2020-05-22 21:44:57 +0900

    feat: UpdateMember argument resolver 추가

--- src/main/java/wooteco/subway/config/WebMvcConfig.java
+++ src/main/java/wooteco/subway/config/WebMvcConfig.java
@@ -4,27 +4,31 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import wooteco.subway.web.member.LoginMemberMethodArgumentResolver;
+import wooteco.subway.web.member.UpdateMemberMethodArgumentResolver;
 import wooteco.subway.web.member.interceptor.BearerAuthInterceptor;
 
 import java.util.List;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+    private final UpdateMemberMethodArgumentResolver updateMemberMethodArgumentResolver;
     private final BearerAuthInterceptor bearerAuthInterceptor;
     private final LoginMemberMethodArgumentResolver loginMemberArgumentResolver;
 
-    public WebMvcConfig(BearerAuthInterceptor bearerAuthInterceptor, LoginMemberMethodArgumentResolver loginMemberArgumentResolver) {
+    public WebMvcConfig(UpdateMemberMethodArgumentResolver updateMemberMethodArgumentResolver, BearerAuthInterceptor bearerAuthInterceptor, LoginMemberMethodArgumentResolver loginMemberArgumentResolver) {
+        this.updateMemberMethodArgumentResolver = updateMemberMethodArgumentResolver;
         this.bearerAuthInterceptor = bearerAuthInterceptor;
         this.loginMemberArgumentResolver = loginMemberArgumentResolver;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(bearerAuthInterceptor).addPathPatterns("/members");
+        registry.addInterceptor(bearerAuthInterceptor).addPathPatterns("/members", "/members/*");
     }
 
     @Override
     public void addArgumentResolvers(List argumentResolvers) {
+        argumentResolvers.add(updateMemberMethodArgumentResolver);
         argumentResolvers.add(loginMemberArgumentResolver);
     }
 }
