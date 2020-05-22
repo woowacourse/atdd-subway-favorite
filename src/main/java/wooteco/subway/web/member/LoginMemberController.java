67d2f73bc7dc5ed5a3e382f7773d290fc1bc5ddf
황준호 <42054054+hwanghe159@users.noreply.github.com>
2020-05-23 01:10:42 +0900

    feat : 본인의 정보를 조회, 수정, 삭제하는 로직 구현

--- src/main/java/wooteco/subway/web/member/LoginMemberController.java
+++ src/main/java/wooteco/subway/web/member/LoginMemberController.java
@@ -7,6 +7,7 @@ import wooteco.subway.service.member.MemberService;
 import wooteco.subway.service.member.dto.LoginRequest;
 import wooteco.subway.service.member.dto.MemberResponse;
 import wooteco.subway.service.member.dto.TokenResponse;
+import wooteco.subway.service.member.dto.UpdateMemberRequest;
 
 @RestController
 public class LoginMemberController {
@@ -25,5 +26,23 @@ public class LoginMemberController {
     @GetMapping("/me/bearer")
     public ResponseEntity<MemberResponse> getMemberOfMineBasic(@LoginMember Member member) {
         return ResponseEntity.ok().body(MemberResponse.of(member));
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<MemberResponse> getOwnMember(@LoginMember Member member) {
+        return ResponseEntity.ok().body(MemberResponse.of(member));
+    }
+
+    @PutMapping("/me")
+    public ResponseEntity<Void> updateOwnMember(@LoginMember Member member, @RequestBody UpdateMemberRequest request) {
+        System.out.println(member.getId() + "에게 " + request.toString() + "으로 수정하려고 함");
+        memberService.updateMember(member.getId(), request);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/me")
+    public ResponseEntity<Void> deleteOwnMember(@LoginMember Member member) {
+        memberService.deleteMember(member.getId());
+        return ResponseEntity.noContent().build();
     }
 }
