bbf703c3c2326bae504ca15f9a77150155fcbe8a
yeonnseok <ljinsk3@gmail.com>
2020-05-24 19:19:26 +0900

    feat: 내 정보 수정 기능

--- src/main/java/wooteco/subway/web/member/LoginMemberController.java
+++ src/main/java/wooteco/subway/web/member/LoginMemberController.java
@@ -1,15 +1,13 @@
 package wooteco.subway.web.member;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import wooteco.subway.domain.member.Member;
 import wooteco.subway.service.member.MemberService;
 import wooteco.subway.service.member.dto.LoginRequest;
 import wooteco.subway.service.member.dto.MemberResponse;
 import wooteco.subway.service.member.dto.TokenResponse;
+import wooteco.subway.service.member.dto.UpdateMemberRequest;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -28,14 +26,22 @@ public class LoginMemberController {
         return ResponseEntity.ok().body(new TokenResponse(token, "Bearer"));
     }
 
+    @GetMapping("/logout")
+    public ResponseEntity<Void> logout(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/login");
+        return ResponseEntity.ok().build();
+    }
+
     @GetMapping("/me/bearer")
     public ResponseEntity<MemberResponse> getMemberOfMineBasic(@LoginMember Member member) {
         return ResponseEntity.ok().body(MemberResponse.of(member));
     }
 
-    @GetMapping("logout")
-    public ResponseEntity<Void> logout(HttpServletResponse response) throws IOException {
-        response.sendRedirect("/login");
+    @PutMapping("/me")
+    public ResponseEntity<Void> update(
+            @LoginMember Member member,
+            @RequestBody UpdateMemberRequest request) {
+        memberService.updateMember(member.getId(), request);
         return ResponseEntity.ok().build();
     }
 }
