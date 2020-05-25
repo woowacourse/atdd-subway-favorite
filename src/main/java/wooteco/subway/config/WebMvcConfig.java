34503d915348d8d484d9b842ac1190138bed652d
hwangbo <taekwonod714@naver.com>
2020-05-25 14:13:11 +0900

    fix: 정보 수정 페이지에서 수정 및 삭제가 안되는 것 수정

--- src/main/java/wooteco/subway/config/WebMvcConfig.java
+++ src/main/java/wooteco/subway/config/WebMvcConfig.java
@@ -21,8 +21,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(bearerAuthInterceptor).addPathPatterns("/me/bearer");
-        registry.addInterceptor(bearerAuthInterceptor).addPathPatterns("/members/*", "me");
-        registry.addInterceptor(bearerAuthInterceptor).addPathPatterns("/mypage", "mypage-edit");
+        registry.addInterceptor(bearerAuthInterceptor).addPathPatterns("/members/*", "/me");
     }
 
     @Override
