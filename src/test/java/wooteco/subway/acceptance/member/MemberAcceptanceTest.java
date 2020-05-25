218cca233b725a43b56cc94d7af610ffbe3122b3
chanin <pci2676@gmail.com>
2020-05-25 20:45:04 +0900

    refactor : 인수테스트 다이나믹 테스트로 변경

--- src/test/java/wooteco/subway/acceptance/member/MemberAcceptanceTest.java
+++ src/test/java/wooteco/subway/acceptance/member/MemberAcceptanceTest.java
@@ -2,7 +2,9 @@ package wooteco.subway.acceptance.member;
 
 import io.restassured.mapper.TypeRef;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import wooteco.subway.AcceptanceTest;
@@ -13,10 +15,45 @@ import wooteco.subway.web.dto.ErrorCode;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
 public class MemberAcceptanceTest extends AcceptanceTest {
+
+    @DisplayName("회원 관리 기능")
+    @TestFactory
+    Stream<DynamicTest> manageMember() {
+        return Stream.of(
+                dynamicTest(
+                        "회원가입", () -> {
+                            String location = createMember(TEST_USER_EMAIL, TEST_USER_NAME, TEST_USER_PASSWORD);
+                            assertThat(location).isNotBlank();
+                        }),
+                dynamicTest(
+                        "내 정보 조회", () -> {
+                            TokenResponse tokenResponse = loginMember(TEST_USER_EMAIL, TEST_USER_PASSWORD);
+                            MemberResponse memberResponse = getMember(tokenResponse);
+                            assertThat(memberResponse.getId()).isNotNull();
+                            assertThat(memberResponse.getEmail()).isEqualTo(TEST_USER_EMAIL);
+                            assertThat(memberResponse.getName()).isEqualTo(TEST_USER_NAME);
+                        }),
+                dynamicTest(
+                        "내 정보 수정", () -> {
+                            TokenResponse tokenResponse = loginMember(TEST_USER_EMAIL, TEST_USER_PASSWORD);
+                            updateMember(tokenResponse);
+                            MemberResponse updatedMember = getMember(tokenResponse);
+                            assertThat(updatedMember.getName()).isEqualTo("NEW_" + TEST_USER_NAME);
+                        }),
+                dynamicTest("회원 탈퇴", () -> {
+                    TokenResponse tokenResponse = loginMember(TEST_USER_EMAIL, "NEW_" + TEST_USER_PASSWORD);
+                    deleteMember(tokenResponse);
+                })
+        );
+
+    }
+
     @DisplayName("중복된 이메일로 회원가입 실패시 Exception 발생")
     @Test
     void createMember() {
@@ -41,25 +78,6 @@ public class MemberAcceptanceTest extends AcceptanceTest {
         assertThat(defaultResponse.getCode()).isEqualTo(ErrorCode.MEMBER_DUPLICATED_EMAIL.getCode());
         assertThat(defaultResponse.getData()).isNull();
         assertThat(defaultResponse.getMessage()).isEqualTo(ErrorCode.MEMBER_DUPLICATED_EMAIL.getMessage());
-    }
-
-    @DisplayName("회원 관리 기능")
-    @Test
-    void manageMember() {
-        String location = createMember(TEST_USER_EMAIL, TEST_USER_NAME, TEST_USER_PASSWORD);
-        assertThat(location).isNotBlank();
-
-        TokenResponse tokenResponse = loginMember(TEST_USER_EMAIL, TEST_USER_PASSWORD);
-        MemberResponse memberResponse = getMember(tokenResponse);
-        assertThat(memberResponse.getId()).isNotNull();
-        assertThat(memberResponse.getEmail()).isEqualTo(TEST_USER_EMAIL);
-        assertThat(memberResponse.getName()).isEqualTo(TEST_USER_NAME);
-
-        updateMember(tokenResponse);
-        MemberResponse updatedMember = getMember(tokenResponse);
-        assertThat(updatedMember.getName()).isEqualTo("NEW_" + TEST_USER_NAME);
-
-        deleteMember(tokenResponse);
     }
 
     @DisplayName("로그인 하지 않은 상태에서 멤버 정보 조회")
