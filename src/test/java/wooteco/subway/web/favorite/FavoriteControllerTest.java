080337237d62667969f397b6d4a1cee94ca868e3
kouz <lastpuzly@gmail.com>
2020-05-26 14:37:38 +0900

    feat: 즐겨찾기 api 문서화

--- src/test/java/wooteco/subway/web/favorite/FavoriteControllerTest.java
+++ src/test/java/wooteco/subway/web/favorite/FavoriteControllerTest.java
@@ -2,6 +2,7 @@ package wooteco.subway.web.favorite;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -11,16 +12,21 @@ import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
+import wooteco.subway.doc.FavoriteDocumentation;
 import wooteco.subway.infra.JwtTokenProvider;
 import wooteco.subway.service.favorite.FavoriteService;
 import wooteco.subway.service.favorite.dto.FavoriteResponse;
@@ -29,6 +35,7 @@ import wooteco.subway.service.member.dto.MemberResponse;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ExtendWith(RestDocumentationExtension.class)
 class FavoriteControllerTest {
     private final String uri = "/favorites";
 
@@ -45,8 +52,12 @@ class FavoriteControllerTest {
     protected MockMvc mockMvc;
 
     @BeforeEach
-    public void setUp(WebApplicationContext webApplicationContext) {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    public void setUp(WebApplicationContext webApplicationContext,
+        RestDocumentationContextProvider restDocumentation) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+            .addFilter(new ShallowEtagHeaderFilter())
+            .apply(documentationConfiguration(restDocumentation))
+            .build();
     }
 
     @Test
@@ -66,7 +77,7 @@ class FavoriteControllerTest {
             .andDo(print())
             .andExpect(status().isCreated())
             .andExpect(header().exists("Location"))
-            .andReturn();
+            .andDo(FavoriteDocumentation.createFavorite());
     }
 
     @Test
@@ -83,6 +94,7 @@ class FavoriteControllerTest {
             .header("Authorization", "bearer " + token))
             .andDo(print())
             .andExpect(status().isOk())
+            .andDo(FavoriteDocumentation.showFavorites())
             .andReturn();
 
         assertThat(mvcResult.getResponse().getContentType()).isEqualTo(MediaType.APPLICATION_JSON_VALUE);
@@ -98,6 +110,6 @@ class FavoriteControllerTest {
             .contentType(MediaType.APPLICATION_JSON))
             .andDo(print())
             .andExpect(status().isOk())
-            .andReturn();
+            .andDo(FavoriteDocumentation.deleteFavorite());
     }
 }
