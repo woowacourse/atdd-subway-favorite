a50661cdb6344486bf6f31fbf18e227001248e3f
Ramen6315 <ramen6315@gmail.com>
2020-05-26 13:52:45 +0900

    refactor: FavoriteResponse ID 추가
    개별적인 Favorite 요청을 식별하기 위해 ID 값을 DTO에 추가

--- src/main/java/wooteco/subway/service/favorite/dto/FavoriteResponse.java
+++ src/main/java/wooteco/subway/service/favorite/dto/FavoriteResponse.java
@@ -3,16 +3,22 @@ package wooteco.subway.service.favorite.dto;
 import wooteco.subway.domain.favorite.Favorite;
 
 public class FavoriteResponse {
+    private final Long id;
     private final String source;
     private final String target;
 
-    public FavoriteResponse(String source, String target) {
+    public FavoriteResponse(Long id, String source, String target) {
+        this.id = id;
         this.source = source;
         this.target = target;
     }
 
     public static FavoriteResponse of(Favorite favorite) {
-        return new FavoriteResponse(favorite.getSource(), favorite.getTarget());
+        return new FavoriteResponse(favorite.getId(), favorite.getSource(), favorite.getTarget());
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getSource() {
