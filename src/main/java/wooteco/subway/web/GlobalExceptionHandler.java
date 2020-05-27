c9bfa469d34d75f7da1c5aabee92aef083d26368
lalize <printf@kakao.com>
2020-05-27 16:34:47 +0900

    feat: 예외 핸들러 추가

--- src/main/java/wooteco/subway/web/GlobalExceptionHandler.java
+++ src/main/java/wooteco/subway/web/GlobalExceptionHandler.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import wooteco.subway.service.member.exception.DuplicateMemberException;
+import wooteco.subway.service.member.exception.IncorrectPasswordException;
+import wooteco.subway.service.member.exception.NotFoundMemberException;
 import wooteco.subway.web.dto.ErrorResponse;
 import wooteco.subway.web.member.InvalidAuthenticationException;
 
@@ -24,15 +26,27 @@ public class GlobalExceptionHandler {
         return ResponseEntity.badRequest().body(errorResponse);
     }
 
+    @ExceptionHandler(IncorrectPasswordException.class)
+    public ResponseEntity<ErrorResponse> handleIncorrectPasswordException() {
+        ErrorResponse errorResponse = new ErrorResponse("패스워드가 일치하지 않습니다.");
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+
+    @ExceptionHandler(NotFoundMemberException.class)
+    public ResponseEntity<ErrorResponse> handleNotFoundMemberException() {
+        ErrorResponse errorResponse = new ErrorResponse("존재하지 않는 회원입니다.");
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+
     @ExceptionHandler(InvalidAuthenticationException.class)
     public ResponseEntity<ErrorResponse> handleInvalidAuthenticationException() {
         ErrorResponse errorResponse = new ErrorResponse("토큰이 유효하지 않습니다.");
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResponse);
     }
 
-    // @ExceptionHandler(Exception.class)
-    // public ResponseEntity<ErrorResponse> handleException() {
-    //     return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-    //         .body(new ErrorResponse("서버에서 오류가 발생했습니다."));
-    // }
+    @ExceptionHandler(Exception.class)
+     public ResponseEntity<ErrorResponse> handleException() {
+         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+             .body(new ErrorResponse("서버에서 오류가 발생했습니다."));
+     }
 }
