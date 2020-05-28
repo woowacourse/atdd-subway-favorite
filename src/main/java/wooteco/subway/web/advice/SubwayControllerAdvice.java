6f905693e294d1fc783f5cdeca2d3cb9d772cab3
toneyparky <qkrtmddhks95@gmail.com>
2020-05-29 08:21:20 +0900

    refactor: 에러 메시지에 대한 가독성 증가를 위한 수정

--- src/main/java/wooteco/subway/web/advice/SubwayControllerAdvice.java
+++ src/main/java/wooteco/subway/web/advice/SubwayControllerAdvice.java
@@ -12,10 +12,11 @@ import wooteco.subway.exception.*;
 @RestControllerAdvice
 public class SubwayControllerAdvice {
 	private static final Logger LOGGER = LogManager.getLogger("SubwayControllerAdvice");
+	private static final String ERROR_MESSAGE_TEMPLATE = "ERROR MESSAGE >>>>>{}";
 
 	@ExceptionHandler(NoResourceExistException.class)
 	public ResponseEntity<ExceptionResponse> handleNoResourceExistException(NoResourceExistException e) {
-		LOGGER.error(e);
+		LOGGER.error(ERROR_MESSAGE_TEMPLATE, e.getMessage(), e);
 		return new ResponseEntity<>(ExceptionResponse.of(e.getMessage()), HttpStatus.NOT_FOUND);
 	}
 
@@ -27,19 +28,19 @@ public class SubwayControllerAdvice {
 			DuplicatedEmailException.class
 	})
 	public ResponseEntity<ExceptionResponse> handleBadRequestException(RuntimeException e) {
-		LOGGER.error(e);
+		LOGGER.error(ERROR_MESSAGE_TEMPLATE, e.getMessage(), e);
 		return new ResponseEntity<>(ExceptionResponse.of(e.getMessage()), HttpStatus.BAD_REQUEST);
 	}
 
 	@ExceptionHandler(InvalidAuthenticationException.class)
 	public ResponseEntity<ExceptionResponse> handleAuthenticationException(Exception e) {
-		LOGGER.error(e);
+		LOGGER.error(ERROR_MESSAGE_TEMPLATE, e.getMessage(), e);
 		return new ResponseEntity<>(ExceptionResponse.of(e.getMessage()), HttpStatus.UNAUTHORIZED);
 	}
 
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<ExceptionResponse> handleUnexpectedException(Exception e) {
-		LOGGER.error(e);
+		LOGGER.error(ERROR_MESSAGE_TEMPLATE, e.getMessage(), e);
 		return new ResponseEntity<>(ExceptionResponse.of("서버 오류가 발생했어요."), HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
