68f688e997f7f1586b1be9d01eb18c66aeaa44ab
hyochan-kim <summacum94@naver.com>
2020-05-28 23:32:48 +0900

    feat: commonException / exception handler 추가

--- src/main/java/wooteco/subway/service/member/MemberService.java
+++ src/main/java/wooteco/subway/service/member/MemberService.java
@@ -1,11 +1,13 @@
 package wooteco.subway.service.member;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import wooteco.subway.domain.member.Favorite;
 import wooteco.subway.domain.member.Member;
 import wooteco.subway.domain.member.MemberRepository;
 import wooteco.subway.domain.station.Station;
 import wooteco.subway.domain.station.StationRepository;
+import wooteco.subway.exception.CommonException;
 import wooteco.subway.infra.JwtTokenProvider;
 import wooteco.subway.service.member.dto.*;
 
@@ -29,7 +31,8 @@ public class MemberService {
     }
 
     public void updateMember(Long id, UpdateMemberRequest param) {
-        Member member = memberRepository.findById(id).orElseThrow(RuntimeException::new);
+        Member member = memberRepository.findById(id)
+                .orElseThrow(()-> new CommonException(HttpStatus.BAD_REQUEST, "Not exist member id = " + id));
         member.update(param.getName(), param.getPassword());
         memberRepository.save(member);
     }
@@ -39,16 +42,18 @@ public class MemberService {
     }
 
     public String createToken(LoginRequest param) {
-        Member member = memberRepository.findByEmail(param.getEmail()).orElseThrow(RuntimeException::new);
+        Member member = memberRepository.findByEmail(param.getEmail())
+                .orElseThrow(()-> new CommonException(HttpStatus.BAD_REQUEST, "Not exist member email = " + param.getEmail()));
         if (!member.checkPassword(param.getPassword())) {
-            throw new RuntimeException("잘못된 패스워드");
+            throw new CommonException(HttpStatus.UNAUTHORIZED, "잘못된 패스워드");
         }
 
         return jwtTokenProvider.createToken(member.getId() + ":" + param.getEmail());
     }
 
     public Member findMemberByEmail(String email) {
-        return memberRepository.findByEmail(email).orElseThrow(RuntimeException::new);
+        return memberRepository.findByEmail(email)
+                .orElseThrow(()-> new CommonException(HttpStatus.BAD_REQUEST, "Not exist member email = " + email));
     }
 
     public boolean loginWithForm(String email, String password) {
@@ -57,7 +62,8 @@ public class MemberService {
     }
 
     public MemberFavoriteResponse findFavorites(Long id) {
-        Member member = memberRepository.findById(id).orElseThrow(IllegalArgumentException::new);
+        Member member = memberRepository.findById(id)
+                .orElseThrow(()-> new CommonException(HttpStatus.BAD_REQUEST, "Not exist member id = " + id));
 
         Set<Favorite> favorites = member.getFavorites();
         Set<Long> stationIds = new HashSet<>();
@@ -83,7 +89,8 @@ public class MemberService {
     }
 
     public void addFavorite(Long id, FavoriteCreateRequest favoriteCreateRequest) {
-        Member member = memberRepository.findById(id).orElseThrow(IllegalArgumentException::new);
+        Member member = memberRepository.findById(id)
+                .orElseThrow(()-> new CommonException(HttpStatus.BAD_REQUEST, "Not exist member id = " + id));
         member.addFavorite(new Favorite(favoriteCreateRequest.getStartStationId(),
                 favoriteCreateRequest.getEndStationId()));
         memberRepository.save(member);
@@ -91,7 +98,8 @@ public class MemberService {
 
 
     public void deleteFavoriteById(Long memberId, Long favoriteId) {
-        Member member = memberRepository.findById(memberId).orElseThrow(IllegalArgumentException::new);
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(()-> new CommonException(HttpStatus.BAD_REQUEST, "Not exist member id = " + memberId));
         member.removeFavorite(favoriteId);
         memberRepository.save(member);
     }
