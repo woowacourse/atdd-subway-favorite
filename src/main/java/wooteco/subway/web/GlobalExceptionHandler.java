a3045e3b715d9a1a80d67ac85f0fa5fd562d7ee7
jamie9504 <jamie9504@kakao.com>
2020-05-28 10:35:03 +0900

    feat : 예기치 못한 예외 핸들링

--- src/main/java/wooteco/subway/web/GlobalExceptionHandler.java
+++ src/main/java/wooteco/subway/web/GlobalExceptionHandler.java
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
 import wooteco.subway.exception.DuplicateEmailException;
 import wooteco.subway.exception.EntityNotFoundException;
 import wooteco.subway.exception.LoginFailException;
@@ -16,25 +15,32 @@ import wooteco.subway.web.member.InvalidAuthenticationException;
 @RestControllerAdvice(annotations = RestController.class)
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleEntityNotFound(EntityNotFoundException e) {
-        return new ResponseEntity<>(new ErrorResponse(e.getMessage()), HttpStatus.NOT_FOUND);
-    }
+	@ExceptionHandler(EntityNotFoundException.class)
+	public ResponseEntity<ErrorResponse> handleEntityNotFound(EntityNotFoundException e) {
+		return new ResponseEntity<>(new ErrorResponse(e.getMessage()), HttpStatus.NOT_FOUND);
+	}
 
-    @ExceptionHandler(LoginFailException.class)
-    public ResponseEntity<ErrorResponse> handleLoginFail(LoginFailException e) {
-        return new ResponseEntity<>(new ErrorResponse(e.getMessage()), HttpStatus.UNAUTHORIZED);
-    }
+	@ExceptionHandler(LoginFailException.class)
+	public ResponseEntity<ErrorResponse> handleLoginFail(LoginFailException e) {
+		return new ResponseEntity<>(new ErrorResponse(e.getMessage()), HttpStatus.UNAUTHORIZED);
+	}
 
-    @ExceptionHandler(InvalidAuthenticationException.class)
-    public ResponseEntity<ErrorResponse> handleInvalidAuthentication(InvalidAuthenticationException e) {
-        return ResponseEntity.status(HttpStatus.FOUND)
-            .header(HttpHeaders.LOCATION, "/login")
-            .body(new ErrorResponse(e.getMessage()));
-    }
+	@ExceptionHandler(InvalidAuthenticationException.class)
+	public ResponseEntity<ErrorResponse> handleInvalidAuthentication(
+		InvalidAuthenticationException e) {
+		return ResponseEntity.status(HttpStatus.FOUND)
+			.header(HttpHeaders.LOCATION, "/login")
+			.body(new ErrorResponse(e.getMessage()));
+	}
 
-    @ExceptionHandler(DuplicateEmailException.class)
-    public ResponseEntity<ErrorResponse> handleDuplicateEmail(DuplicateEmailException e) {
-        return new ResponseEntity<>(new ErrorResponse(e.getMessage()), HttpStatus.BAD_REQUEST);
-    }
+	@ExceptionHandler(DuplicateEmailException.class)
+	public ResponseEntity<ErrorResponse> handleDuplicateEmail(DuplicateEmailException e) {
+		return new ResponseEntity<>(new ErrorResponse(e.getMessage()), HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ErrorResponse> handleRuntime(Exception e) {
+		return new ResponseEntity<>(new ErrorResponse("예기치 않은 오류가 발생하였습니다."),
+			HttpStatus.BAD_REQUEST);
+	}
 }
