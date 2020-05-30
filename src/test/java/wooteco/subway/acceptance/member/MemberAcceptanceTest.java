784f1b2c50af5b2fae80a198b5390e9fb438bda0
김예림 <kiel0103@naver.com>
2020-05-30 12:39:23 +0900

    test: 테스트 코드 변수명 수정

--- src/test/java/wooteco/subway/acceptance/member/MemberAcceptanceTest.java
+++ src/test/java/wooteco/subway/acceptance/member/MemberAcceptanceTest.java
@@ -29,14 +29,14 @@ public class MemberAcceptanceTest extends AcceptanceTest {
         final String TEST_PASSWORD = "testPassword";
         createMember(TEST_EMAIL, TEST_NAME, TEST_PASSWORD);
 
-        Map<String, String> params = new HashMap<>();
-        params.put("email", TEST_EMAIL);
-        params.put("name", TEST_NAME);
-        params.put("password", TEST_PASSWORD);
+        Map<String, String> requestBody = new HashMap<>();
+        requestBody.put("email", TEST_EMAIL);
+        requestBody.put("name", TEST_NAME);
+        requestBody.put("password", TEST_PASSWORD);
 
         MemberErrorResponse response =
             given().
-                body(params).
+                body(requestBody).
                 contentType(MediaType.APPLICATION_JSON_VALUE).
                 accept(MediaType.APPLICATION_JSON_VALUE).
             when().
@@ -55,18 +55,19 @@ public class MemberAcceptanceTest extends AcceptanceTest {
         final String TEST_NAME = "";
         final String TEST_PASSWORD = "testPassword";
 
-        Map<String, String> params = new HashMap<>();
-        params.put("email", TEST_EMAIL);
-        params.put("name", TEST_NAME);
-        params.put("password", TEST_PASSWORD);
+        Map<String, String> requestBody = new HashMap<>();
+        requestBody.put("email", TEST_EMAIL);
+        requestBody.put("name", TEST_NAME);
+        requestBody.put("password", TEST_PASSWORD);
 
-        MemberErrorResponse response = given().
-                body(params).
+        MemberErrorResponse response =
+            given().
+                body(requestBody).
                 contentType(MediaType.APPLICATION_JSON_VALUE).
                 accept(MediaType.APPLICATION_JSON_VALUE).
-                when().
+            when().
                 post("/join").
-                then().
+            then().
                 log().all().
                 statusCode(HttpStatus.BAD_REQUEST.value()).
                 extract().as(MemberErrorResponse.class);
@@ -101,6 +102,8 @@ public class MemberAcceptanceTest extends AcceptanceTest {
     @Test
     //todo: csv로 다양한 케이스
     void retrieveMyInfoWithoutLogin() {
+
+        
         String location = createMember(TEST_USER_EMAIL, TEST_USER_NAME, TEST_USER_PASSWORD);
         assertThat(location).isNotBlank();
 
