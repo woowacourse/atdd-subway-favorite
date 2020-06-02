16a084b9294325aac7aa059f9619b2cd13c0236c
KIMSIYOUNG <tldud2404@gmail.com>
2020-06-02 12:59:12 +0900

    refacotor : 예외 발생 부분에 대한 로그 추가

--- src/main/java/wooteco/subway/exception/GlobalExceptionHandler.java
+++ src/main/java/wooteco/subway/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package wooteco.subway.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.relational.core.conversion.DbActionExecutionException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -10,34 +12,41 @@ import wooteco.subway.web.member.InvalidAuthenticationException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    private Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(DbActionExecutionException.class)
     public ResponseEntity<ExceptionResponse> dbActionExecutionException(DbActionExecutionException e) {
+        logger.error("error >> {}", e.getMessage(), e);
         return ResponseEntity.badRequest().body(ExceptionResponse.of(e));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ExceptionResponse> methodArgumentNotValidException(MethodArgumentNotValidException e) {
+        logger.error("error >> {}", e.getMessage(), e);
         return ResponseEntity.badRequest().body(ExceptionResponse.of(e));
     }
 
     @ExceptionHandler(DuplicateEmailException.class)
     public ResponseEntity<ExceptionResponse> duplicatedEmailException(DuplicateEmailException e) {
+        logger.error("error >> {}", e.getMessage(), e);
         return ResponseEntity.badRequest().body(ExceptionResponse.of(e));
     }
 
     @ExceptionHandler(NoSuchMemberException.class)
     public ResponseEntity<ExceptionResponse> noSuchMemberException(NoSuchMemberException e) {
+        logger.error("error >> {}", e.getMessage(), e);
         return ResponseEntity.badRequest().body(ExceptionResponse.of(e));
     }
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<ExceptionResponse> runtimeException(RuntimeException e) {
+        logger.error("error >> {}", e.getMessage(), e);
         return ResponseEntity.status(500).body(ExceptionResponse.of(e));
     }
 
     @ExceptionHandler(InvalidAuthenticationException.class)
     public ResponseEntity<ExceptionResponse> invalidAuthenticationException(InvalidAuthenticationException e) {
+        logger.error("error >> {}", e.getMessage(), e);
         return ResponseEntity.status(401).body(ExceptionResponse.of(e));
     }
 }
