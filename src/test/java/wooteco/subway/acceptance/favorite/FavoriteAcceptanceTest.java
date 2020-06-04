f261e2307796cf3bdbcd00ba69f9f897e2311d8a
chws <rebeccachoosir@gmail.com>
2020-06-04 22:17:54 +0900

    refactor : FavoriteAcceptanceTest에서 given-when-then 주석 추가

--- src/test/java/wooteco/subway/acceptance/favorite/FavoriteAcceptanceTest.java
+++ src/test/java/wooteco/subway/acceptance/favorite/FavoriteAcceptanceTest.java
@@ -21,7 +21,7 @@ public class FavoriteAcceptanceTest extends AcceptanceTest {
     @DisplayName("즐겨찾기 관리(추가, 조회, 삭제) 인수테스트")
     @Test
     public void manageFavorite() {
-        //given
+        //given 회원가입이 되어 있고, 역/노선/구간이 등록되어 있다
         createMember(TEST_USER_EMAIL, TEST_USER_NAME, TEST_USER_PASSWORD);
         StationResponse stationResponse1 = createStation(STATION_NAME_KANGNAM);
         StationResponse stationResponse2 = createStation(STATION_NAME_YEOKSAM);
@@ -33,15 +33,23 @@ public class FavoriteAcceptanceTest extends AcceptanceTest {
         addLineStation(lineResponse.getId(), stationResponse1.getId(), stationResponse2.getId());
         addLineStation(lineResponse.getId(), stationResponse2.getId(), stationResponse3.getId());
 
+        //and 로그인이 되어 있다
         TokenResponse tokenResponse = login(TEST_USER_EMAIL, TEST_USER_PASSWORD);
 
+        //when 즐겨찾기 추가를 한다
         addFavoritePath(tokenResponse, stationResponse1, stationResponse2);
 
+        //when 즐겨찾기 조회 요청을 한다
         List<FavoriteResponse> responses = getFavoritePath(tokenResponse);
+
+        //then 즐겨찾기가 추가되고 조회할 수 있다
         assertThat(responses.get(0).getSourceStationId()).isEqualTo(stationResponse1.getId());
         assertThat(responses.get(0).getTargetStationId()).isEqualTo(stationResponse2.getId());
 
+        //when 즐겨찾기 삭제 요청을 한다
         deleteFavoritePath(tokenResponse, responses.get(0).getId());
+
+        //then 즐겨찾기가 삭제된다
         responses = getFavoritePath(tokenResponse);
         assertThat(responses.size()).isEqualTo(0);
     }
