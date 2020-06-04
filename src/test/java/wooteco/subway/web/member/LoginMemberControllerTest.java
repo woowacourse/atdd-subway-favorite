fa16ac292ce5460916cc9074c91faab1e9499d6f
ordinCode <ysh1346@naver.com>
2020-06-04 22:05:07 +0900

    refactor : LoginMemberControllerTest
     - mock 없이 테스트

--- src/test/java/wooteco/subway/web/member/LoginMemberControllerTest.java
+++ src/test/java/wooteco/subway/web/member/LoginMemberControllerTest.java
@@ -1,38 +1,32 @@
 package wooteco.subway.web.member;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import wooteco.subway.service.member.MemberService;
+import wooteco.subway.domain.member.Member;
+import wooteco.subway.domain.member.MemberRepository;
 
-import static org.mockito.BDDMockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class LoginMemberControllerTest {
-    @MockBean
-    private MemberService memberService;
+
+    @Autowired
+    private MemberRepository memberRepository;
 
     @Autowired
     private MockMvc mockMvc;
 
-    @BeforeEach
-    void setUp() {
-
-    }
-
     @Test
     void createTokenWithError() throws Exception {
-        given(memberService.createToken(any())).willThrow(IllegalArgumentException.class);
+        memberRepository.save(new Member("brown@email.com","brown","brown"));
 
-        String input = "\"email\" : \"brown@email.com\", \"password\" : \"brown\"\n";
+        String input = "\"email\" : \"brown@email.com\", \"password\" : \"wrongPassword\"\n";
         this.mockMvc.perform(post("/oauth/token")
                 .content(input)
                 .contentType(MediaType.APPLICATION_JSON)
