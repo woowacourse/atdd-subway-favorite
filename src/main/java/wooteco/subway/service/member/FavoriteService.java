bb59ae4ba65449c81230f111f7cf7ee9edaf13ae
hwookim <gusdn9754@gmail.com>
2020-06-05 14:49:50 +0900

    refactor: 지하철 역이 삭제된 경우에 대비
     FavoriteService.findFavorite() 시 response로 "삭제된 역"을 표시 후 해당 favorite 삭제
     FavoriteService.deleteFavorite() 시 이미 삭제된 역일 경우 프론트 반영 후 로직 종료

--- src/main/java/wooteco/subway/service/member/FavoriteService.java
+++ src/main/java/wooteco/subway/service/member/FavoriteService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import wooteco.subway.domain.member.Favorite;
 import wooteco.subway.domain.member.Member;
 import wooteco.subway.domain.member.MemberRepository;
+import wooteco.subway.domain.station.Station;
 import wooteco.subway.domain.station.StationRepository;
 import wooteco.subway.service.member.dto.FavoriteDeleteRequest;
 import wooteco.subway.service.member.dto.FavoriteRequest;
@@ -14,6 +15,9 @@ import wooteco.subway.service.member.dto.FavoriteResponse;
 
 @Service
 public class FavoriteService {
+
+    private static final String DELETED_STATION = "삭제된 역";
+
     private final MemberRepository memberRepository;
     private final StationRepository stationRepository;
 
@@ -32,21 +36,34 @@ public class FavoriteService {
     public List<FavoriteResponse> findFavorites(Member member) {
         return member.getFavorites()
             .stream()
-            .map(this::findFavorite)
+            .map(favorite -> findFavorite(member, favorite))
             .collect(Collectors.toList());
     }
 
-    private FavoriteResponse findFavorite(Favorite favorite) {
+    private FavoriteResponse findFavorite(Member member, Favorite favorite) {
         String source = stationRepository.findById(favorite.getSourceId())
-            .orElseThrow(NoSuchElementException::new)
+            .orElseGet(() -> new Station(DELETED_STATION))
             .getName();
         String target = stationRepository.findById(favorite.getTargetId())
-            .orElseThrow(NoSuchElementException::new)
+            .orElseGet(() -> new Station(DELETED_STATION))
             .getName();
+
+        if(isDeleted(source, target)) {
+            member.removeFavorite(favorite);
+            memberRepository.save(member);
+        }
         return new FavoriteResponse(source, target);
     }
 
+    private boolean isDeleted(String source, String target) {
+        return DELETED_STATION.equals(source) || DELETED_STATION.equals(target);
+    }
+
     public void deleteFavorite(Member member, FavoriteDeleteRequest request) {
+        if(isDeleted(request.getSourceName(), request.getTargetName())) {
+            return;
+        }
+
         Favorite favorite = findFavoriteByName(request.getSourceName(), request.getTargetName());
         member.removeFavorite(favorite);
         memberRepository.save(member);
