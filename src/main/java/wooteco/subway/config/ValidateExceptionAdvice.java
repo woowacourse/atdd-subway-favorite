58ed7a866f882e0c83f0485ccdc8a6c68f8fc72e
lxxjn0 <ljy3510@gmail.com>
2020-06-06 17:57:02 +0900

    refactor : 세부적인 예외 상황에 따른 ExceptionHandler 메서드 구현
    - InvalidAuthenticationException, InvalidMemberException, DbActionExecutionException, Exception 에 대한 핸들링 메서드 구현.

--- src/main/java/wooteco/subway/config/ValidateExceptionAdvice.java
+++ src/main/java/wooteco/subway/config/ValidateExceptionAdvice.java
@@ -3,12 +3,14 @@ package wooteco.subway.config;
 import static java.util.stream.Collectors.*;
 
 import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.data.relational.core.conversion.DbActionExecutionException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import wooteco.subway.service.member.InvalidMemberException;
+import wooteco.subway.web.InvalidAuthenticationException;
 
 @RestControllerAdvice
 public class ValidateExceptionAdvice {
@@ -26,8 +28,29 @@ public class ValidateExceptionAdvice {
 			.body(errorMessage);
 	}
 
+	@ExceptionHandler(InvalidAuthenticationException.class)
+	public ResponseEntity<String> handleInvalidAuthenticationException(InvalidAuthenticationException e) {
+		return ResponseEntity
+			.badRequest()
+			.body(e.getMessage());
+	}
+
 	@ExceptionHandler(InvalidMemberException.class)
 	public ResponseEntity<String> handleInvalidMemberException(InvalidMemberException e) {
+		return ResponseEntity
+			.badRequest()
+			.body(e.getMessage());
+	}
+
+	@ExceptionHandler(DbActionExecutionException.class)
+	public ResponseEntity<String> handleDbActionExecutionException(DbActionExecutionException e) {
+		return ResponseEntity
+			.badRequest()
+			.body(e.getMessage());
+	}
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<String> handleUnexpectedException(Exception e) {
 		return ResponseEntity
 			.badRequest()
 			.body(e.getMessage());
