1359fb69685fa9eec9155f8b94666f37f8a98f5c
kouz <lastpuzly@gmail.com>
2020-06-07 17:36:52 +0900

    refactor: Logger 적용

--- src/main/java/wooteco/subway/web/ExceptionController.java
+++ src/main/java/wooteco/subway/web/ExceptionController.java
@@ -1,5 +1,7 @@
 package wooteco.subway.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -14,6 +16,8 @@ import wooteco.subway.service.member.exception.InvalidMemberIdException;
 
 @ControllerAdvice
 public class ExceptionController {
+    private static final Logger logger = LoggerFactory.getLogger(ExceptionController.class.getSimpleName());
+
     @ExceptionHandler(value = {DuplicateEmailException.class, InvalidMemberIdException.class,
             InvalidMemberEmailException.class, DuplicateFavoriteException.class, MethodArgumentNotValidException.class})
     public ResponseEntity<ErrorResponse> getDefined(RuntimeException exception) {
@@ -22,9 +26,7 @@ public class ExceptionController {
 
     @ExceptionHandler(value = Exception.class)
     public ResponseEntity<ErrorResponse> getException(Exception exception) {
-        System.out.println(exception.getMessage());
-        exception.printStackTrace();
-
+        logger.error("Unexpected Error", exception);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse());
     }
 }
