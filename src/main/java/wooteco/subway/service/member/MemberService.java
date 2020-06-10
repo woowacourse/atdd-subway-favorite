bc47098c754cb5bee1e4a36a8be765632079f436
hamliet <hamliet@hanyang.ac.kr>
2020-06-10 22:05:56 +0900

    feat: MemberService 동시 요청 문제 해결

--- src/main/java/wooteco/subway/service/member/MemberService.java
+++ src/main/java/wooteco/subway/service/member/MemberService.java
@@ -1,6 +1,7 @@
 package wooteco.subway.service.member;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import wooteco.subway.domain.member.Member;
 import wooteco.subway.domain.member.MemberRepository;
 import wooteco.subway.infra.JwtTokenProvider;
@@ -21,6 +22,7 @@ public class MemberService {
         this.jwtTokenProvider = jwtTokenProvider;
     }
 
+    @Transactional
     public Member createMember(Member member) {
         memberRepository.findByEmail(member.getEmail())
                 .ifPresent(it -> {
@@ -30,15 +32,18 @@ public class MemberService {
         return memberRepository.save(member);
     }
 
+    @Transactional
     public void updateMember(Member member, UpdateMemberRequest param) {
         member.update(param.getName(), param.getPassword());
         memberRepository.save(member);
     }
 
+    @Transactional
     public void deleteMember(Member member) {
         memberRepository.delete(member);
     }
 
+    @Transactional
     public String createToken(LoginRequest param) {
         Member member = memberRepository.findByEmail(param.getEmail())
                 .orElseThrow(() -> new InvalidAuthenticationException(NO_MEMBER));
@@ -49,6 +54,7 @@ public class MemberService {
         return jwtTokenProvider.createToken(param.getEmail());
     }
 
+    @Transactional(readOnly = true)
     public Member findMemberByEmail(String email) {
         return memberRepository.findByEmail(email)
                 .orElseThrow(() -> new IllegalArgumentException(NOT_EXIST_MEMBER));
