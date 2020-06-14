dc39d61ea3363569280bdbadcf95275bcb3954b0
김효찬 <kom1230@hanyang.ac.kr>
2020-06-15 01:06:37 +0900

    refactor: commonException 세분화

--- src/main/java/wooteco/subway/advice/ExceptionAdvice.java
+++ src/main/java/wooteco/subway/advice/ExceptionAdvice.java
@@ -1,14 +1,28 @@
 package wooteco.subway.advice;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import wooteco.subway.exception.CommonException;
+import wooteco.subway.exception.InvalidPasswordException;
+import wooteco.subway.exception.NotExistException;
 
 @RestControllerAdvice
 public class ExceptionAdvice {
-    @ExceptionHandler(CommonException.class)
-    public ResponseEntity handleCommonException(CommonException commonException) {
-        return new ResponseEntity<>(commonException.getMessage(), commonException.getHttpStatus());
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity handleException(Exception exception) {
+        return ResponseEntity.badRequest().build();
+    }
+
+    @ExceptionHandler(NotExistException.class)
+    public ResponseEntity handleNotExsistException(NotExistException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(InvalidPasswordException.class)
+    public ResponseEntity handleNotExsistException(InvalidPasswordException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.UNAUTHORIZED);
     }
 }
