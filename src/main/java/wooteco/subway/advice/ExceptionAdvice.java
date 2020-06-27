860f03bbef95af6e16eb1ed28c5f7d5716ae19b4
김효찬 <kom1230@hanyang.ac.kr>
2020-06-27 13:54:49 +0900

    feat: exception log 추가

--- src/main/java/wooteco/subway/advice/ExceptionAdvice.java
+++ src/main/java/wooteco/subway/advice/ExceptionAdvice.java
@@ -1,5 +1,7 @@
 package wooteco.subway.advice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -11,24 +13,29 @@ import wooteco.subway.exception.NotExistException;
 
 @RestControllerAdvice
 public class ExceptionAdvice {
+    private Logger logger = LoggerFactory.getLogger(ExceptionAdvice.class);
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity handleException(Exception exception) {
-        return new ResponseEntity<>(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity handleException(Exception e) {
+        logger.error(String.format("error >> {%s}", e));
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
-        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        logger.error(String.format("error >> {%s}", e));
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(NotExistException.class)
-    public ResponseEntity handleNotExistException(NotExistException exception) {
-        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity handleNotExistException(NotExistException e) {
+        logger.error(String.format("error >> {%s}", e));
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(InvalidPasswordException.class)
-    public ResponseEntity handleInvalidPasswordException(InvalidPasswordException exception) {
-        return new ResponseEntity<>(exception.getMessage(), HttpStatus.UNAUTHORIZED);
+    public ResponseEntity handleInvalidPasswordException(InvalidPasswordException e) {
+        logger.error(String.format("error >> {%s}", e));
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
     }
 }
